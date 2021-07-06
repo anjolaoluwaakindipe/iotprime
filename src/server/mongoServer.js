95ae499cc5f80df6f7fa4a0f540ac427133afc1b
Anjolaoluwa Akindipe <anjyakindipe@gmail.com>
2021-07-06 06:53:19 +0100

    fixing deployment build

--- src/server/mongoServer.js
+++ src/server/mongoServer.js
@@ -6,22 +6,26 @@ const activateMongoServer = async () => {
   let DB_URL;
   if (process.env.NODE_ENV === 'development') {
     DB_URL = process.env.DB_URL_DEVELOPMENT;
-  }
-  if (process.env.NODE_ENV === 'production') {
+  } else {
     DB_URL = process.env.DB_URL_PRODUCTION;
   }
-
-  await mongoose.connect(
-    DB_URL,
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log('connected to a mongodb server');
-    }
-  );
+  try {
+    await mongoose.connect(
+      DB_URL,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => {
+        console.log('connected to a mongodb server');
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
 
   mongoose.connection.on('error', (err) => {
     console.log(err);
   });
 };
 
-module.exports = activateMongoServer;
+module.exports = {
+  activateMongoServer,
+};
